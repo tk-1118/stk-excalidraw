43ada13e522447976cba14d782baee6d2b831b69
ShiTengKai <fyj2827297410@163.com>
2025-07-23 18:05:48 +0800

    添加frame的png导出功能

--- packages/excalidraw/components/BusinessServiceProtoNav/BusinessServiceProtoNav.tsx
+++ packages/excalidraw/components/BusinessServiceProtoNav/BusinessServiceProtoNav.tsx
@@ -1,58 +1,111 @@
-import './BusinessServiceProtoNav.scss'
+import "./BusinessServiceProtoNav.scss";
 
-import { useState } from 'react'
+import { exportToCanvas } from "@excalidraw/utils/export";
 
-import { frameToolIcon } from '../icons'
+import { useState } from "react";
 
-import { useApp } from '../App'
 import { isFrameLikeElement } from "@excalidraw/element";
 
-import type {
-    ExcalidrawFrameLikeElement,
-} from "@excalidraw/element/types";
+import { getDefaultFrameName, getElementsOverlappingFrame } from "@excalidraw/element/frame";
 
-import { getDefaultFrameName } from "@excalidraw/element/frame";
+import type { ExcalidrawFrameLikeElement } from "@excalidraw/element/types";
 
+import { frameToolIcon, ExportIcon } from "../icons";
+
+import { useApp } from "../App";
 
 export const BusinessServiceProtoNav = () => {
-    const app = useApp();
-    const elements = app.scene.getNonDeletedElements();
-    console.log('elements:', elements);
-    
-    const frames = elements.filter((el) =>
-        isFrameLikeElement(el),
-    ) as ExcalidrawFrameLikeElement[];
-    frames.sort((a, b) => a.y - b.y);
-    console.log('frames:', frames);
+  const app = useApp();
+  const elements = app.scene.getNonDeletedElements();
+  //   console.log("elements:", elements);
 
-    const [selectedFrame, setSelectedFrame] = useState<ExcalidrawFrameLikeElement | null>(null);
-    const frameClick = (frame: ExcalidrawFrameLikeElement) => {
-        console.log('frame:', frame);
-        setSelectedFrame(frame);
-        app.scrollToContent(frame, { animate: true })
-    }
-    
+  const frames = elements.filter((el) =>
+    isFrameLikeElement(el),
+  ) as ExcalidrawFrameLikeElement[];
+  frames.sort((a, b) => a.y - b.y);
+  //   console.log("frames:", frames);
 
-    return (
-        <>
-            <div className="business-service-proto-nav">
-                <div className="business-service-proto-nav-header">
-                    <h4 className='business-service-proto-design'>业务服务原型设计</h4>
-                    <h2 className='business-service-proto-name'>业务服务名称</h2>
+  const [selectedFrame, setSelectedFrame] =
+    useState<ExcalidrawFrameLikeElement | null>(null);
+  const frameClick = (frame: ExcalidrawFrameLikeElement) => {
+    // console.log("frame:", frame);
+    setSelectedFrame(frame);
+    app.scrollToContent(frame, { animate: true });
+  };
+
+  const frameExportPng = async (exportingFrame: ExcalidrawFrameLikeElement) => {
+    const exportedElements = getElementsOverlappingFrame(
+      elements,
+      exportingFrame,
+    );
+    const canvas = exportToCanvas({
+      elements: exportedElements,
+      appState: app.state,
+      files: app.files,
+      exportingFrame, // 关键参数
+      exportPadding: 0,
+    });
+    // 转换为 PNG blob
+    const blob = await new Promise<Blob>(async (resolve) => {
+      (await canvas).toBlob((blob) => resolve(blob!), "image/png");
+    });
+    // 创建一个临时的 URL 对象
+    const url = URL.createObjectURL(blob);
+
+    // 创建一个隐藏的 <a> 标签
+    const a = document.createElement("a");
+    a.style.display = "none";
+    a.href = url;
+    a.download = exportingFrame.name || "";
+
+    // 将 <a> 标签添加到文档中
+    document.body.appendChild(a);
+    a.click();
+
+    // 清理
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+  };
+  return (
+    <>
+      <div className="business-service-proto-nav">
+        <div className="business-service-proto-nav-header">
+          <h4 className="business-service-proto-design">业务服务原型设计</h4>
+          <h2 className="business-service-proto-name">业务服务名称</h2>
+        </div>
+        <div className="business-service-proto-nav-body">
+          <div className="business-service-proto-nav-body-frames">
+            {(frames || []).map((frame) => (
+              <div
+                key={frame.id}
+                className={`business-service-proto-nav-body-frames-item ${
+                  selectedFrame?.id === frame.id ? "active" : ""
+                }`}
+                onClick={() => {
+                  frameClick(frame);
+                }}
+              >
+                <div className="frames-item-left">
+                  <div className="title-icon">{frameToolIcon}</div>
+                  <div className="title-text">
+                    {frame.name ?? getDefaultFrameName(frame)}
+                  </div>
                 </div>
-                <div className="business-service-proto-nav-body">
-                    <div className="business-service-proto-nav-body-frames">
-                        {(frames || []).map((frame) => (
-                            <div key={frame.id} className={`business-service-proto-nav-body-frames-item ${selectedFrame?.id === frame.id ? 'active' : ''}`} onClick={() => {
-                                frameClick(frame);
-                            }}>
-                                <div className="title-icon">{frameToolIcon}</div>
-                                <div className="title-text">{frame.name ?? getDefaultFrameName(frame)}</div>
-                            </div>
-                        ))}
-                    </div>
+                <div className="frames-item-right">
+                  <div
+                    className="export-icon"
+                    onClick={() => {
+                      frameExportPng(frame);
+                    }}
+                  >
+                    {ExportIcon}
+                  </div>
                 </div>
-            </div>
-        </>
-    )
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
